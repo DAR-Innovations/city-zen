067698383ce6baed77c1674930c0cd1aa742f038
DiarCode <diar_beg64@mail.ru>
2025-01-08 12:52:51 +0500

    feature: added shadcn, tailwind, router

---
+++ frontend/src/core/constants/seo.constant.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_TITLE = 'CityZen | Modern Civic Engagement Platform'
+export const TITLE_TEMPLATE = (title: string) => `${title} | CityZen`
+export const DEFAULT_DESCRIPTION =
+  'CityZen is a modern civic engagement platform designed to empower citizens and city officials alike by streamlining the process of identifying, reporting, and resolving urban infrastructure issues. With a focus on efficiency, transparency, and smart analytics, CityZen leverages AI-driven tools and user-friendly interfaces to improve city life.'
---
+++ frontend/src/modules/landing/pages/landing-page.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-red-500">Hello World</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
