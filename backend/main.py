edf1173dc03e244c76c29606b71c71d42a8545d6
maulerrr <211239@astanait.edu.kz>
2025-01-09 13:34:06 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,23 +3,7 @@ from app.db.database import engine, get_db
 from app.models.base import Base
 from app.router import api_router
 import uvicorn
-
-
-app = FastAPI()
-
-def init_db():
-    """
-    Initialize the database by creating all tables.
-    This function should only be used in development or testing.
-    """
-    Base.metadata.create_all(bind=engine)
-
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.db.database import engine
-from app.models.base import Base
-from app.router import api_router
-
 from app.models.departments.departments import Departments
 from app.models.agents.employees import Employees
 from app.models.agents.users import Users
@@ -29,6 +13,15 @@ from app.models.events.issue import Issues
 from app.models.events.reports import Reports
 from app.models.tasks.volunteer_tasks import VolunteerTasks
 from app.models.tasks.department_tasks import DepartmentTasks
+
+app = FastAPI()
+
+def init_db():
+    """
+    Initialize the database by creating all tables.
+    This function should only be used in development or testing.
+    """
+    Base.metadata.create_all(bind=engine)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
